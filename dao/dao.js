61978be2dc76959677cda0bf67cab42dd409fece
yumuxu <yumuxu@gmail.com>
2013-04-08 19:21:07 +0800

    v1.1

--- dao/dao.js
+++ dao/dao.js
@@ -4,6 +4,34 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 var dbstr = require("../configure").db;
 
+//Appfog
+if (process.env.VCAP_SERVICES){
+	var env = JSON.parse(process.env.VCAP_SERVICES);
+	var mongo = env['mongodb-1.8'][0]['credentials'];
+}else{
+	var mongo = {
+	"hostname":"localhost",
+	"port":27017,
+	"username":"",
+	"password":"",
+	"name":"",
+	"db":"taotea"
+	}
+}
+var generater_mongo_url = function(obj){
+	obj.hostname = (obj.hostname || 'localhost');
+	obj.port = (obj.port || 27017);
+	obj.db = (obj.db ||'taotea');
+	
+	if(obj.username && obj.password){
+		return "mongodb://"+obj.username + ":" + obj.password+"@"+obj.hostname+":"+obj.port+"/"+obj.db;
+	}else{
+		return "mongodb://"+obj.hostname+":"+obj.port+"/"+obj.db;
+	}
+}
+
+var mongourl = generater_mongo_url(mongo);
+
 //用户模式
 var userschema = new Schema({
 	usrname:String
@@ -30,7 +58,7 @@ var Blogmodel = mongoose.model('blog',blogschema,'blog');
 var Itemmodel = mongoose.model('item',itemschema,'item');
 
 exports.connect = function(callback){
-	mongoose.connect(dbstr);
+	mongoose.connect(mongourl);
 }
 
 exports.disconnect = function(callback){
